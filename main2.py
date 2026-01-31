9ef9321751453580363d7a18694184b1e8003cc1
Akula Raghavendra <akula_raghavendra@Akulas-MacBook-Pro.local>
2026-01-31 12:06:18 +0530

    remodelling the main file for easy understanding

--- main2.py
+++ main2.py
@@ -1,80 +1,9 @@
 from fastapi import FastAPI, status
 from fastapi.exceptions import HTTPException
-from pydantic import BaseModel
+from schemas import Book, updateBookModel
+from book_data import Books
 from typing import List
 
-Books = [
-    {
-        "id": 1,
-        "title": "The Great Gatsby",
-        "author": "F. Scott Fitzgerald",
-        "publisher": "Scribner",
-        "published_date": "1925-04-10",
-        "page_count": 218,
-        "language": "English"
-    },
-    {
-        "id": 2,
-        "title": "To Kill a Mockingbird",
-        "author": "Harper Lee",
-        "publisher": "J.B. Lippincott & Co.",
-        "published_date": "1960-07-11",
-        "page_count": 281,
-        "language": "English"
-    },
-    {
-        "id": 3,
-        "title": "1984",
-        "author": "George Orwell",
-        "publisher": "Secker & Warburg",
-        "published_date": "1949-06-08",
-        "page_count": 328,
-        "language": "English"
-    },
-    {
-        "id": 4,
-        "title": "Pride and Prejudice",
-        "author": "Jane Austen",
-        "publisher": "T. Egerton",
-        "published_date": "1813-01-28",
-        "page_count": 279,
-        "language": "English"
-    },
-    {
-        "id": 5,
-        "title": "The Catcher in the Rye",
-        "author": "J.D. Salinger",
-        "publisher": "Little, Brown and Company",
-        "published_date": "1951-07-16",
-        "page_count": 214,
-        "language": "English"
-    },
-    {
-        "id": 6,
-        "title": "The Hobbit",
-        "author": "J.R.R. Tolkien",
-        "publisher": "George Allen & Unwin",
-        "published_date": "1937-09-21",
-        "page_count": 310,
-        "language": "English"
-    }
-]
-
-class Book (BaseModel):
-    id: int   
-    title: str
-    author: str
-    publisher: str
-    published_date: str
-    page_count: int
-    language: str
-
-class updateBookModel (BaseModel):
-    title: str
-    author: str
-    publisher: str
-    page_count: int
-    language: str
 
 app = FastAPI()
 @app.get("/books", response_model=List[Book])
