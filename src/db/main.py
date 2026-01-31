1251b35ce481a96dd161c16a7952305d959a1c71
Akula Raghavendra <akula_raghavendra@Akulas-MacBook-Pro.local>
2026-01-31 18:53:11 +0530

    Connnected to postgreSQL Bookdb Database Successfully

---
+++ src/db/main.py
@@ -0,0 +1,20 @@
+from sqlmodel import create_engine, text
+from sqlalchemy.ext.asyncio import AsyncEngine
+from src.config import config
+
+engine = AsyncEngine(
+     create_engine(
+    url=config.DATABASE_URL,
+    echo=True
+))
+
+# async fucntion to initialize the database
+async def init_db():
+    async with engine.begin() as conn:
+        statement = text("SELECT 'hello';")
+
+        result=await conn.execute(statement)
+
+        print(result.all)
+    return
+    
